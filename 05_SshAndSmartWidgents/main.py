d5620c8e5e431de68979efde1b4a5faad09929de
Dmitry Lyukov <d@lyukov.com>
2021-03-28 20:54:16 +0300

    Add AutoFrame class. Create application with stub event handlers.

--- 05_SshAndSmartWidgents/main.py
+++ 05_SshAndSmartWidgents/main.py
@@ -16,34 +16,59 @@ object_description = re.compile(
 )
 
 
-class GraphicalEditor(tk.Frame):
-    def __init__(self):
-        pass
+class AutoFrame(tk.Frame):
+    def __init__(self, master=None, **kwargs):
+        '''Create root window with frame, tune weight and resize'''
+        super().__init__(master, **kwargs)
+        self.grid(sticky="NEWS")
+        self.create_widgets()
+        for column in range(self.grid_size()[0]):
+            self.columnconfigure(column, weight=1)
+        for row in range(self.grid_size()[1]):
+            self.rowconfigure(row, weight=1)
+
+    def create_widgets(self):
+        '''Create all the widgets'''
+
+
+class GraphicalEditor(AutoFrame):
+    def create_widgets(self):
+        self.canvas = tk.Canvas(self)
+        self.canvas.bind("<Button-1>", print)
+        self.canvas.bind("<ButtonRelease-1>", print)
+        self.canvas.bind("<Motion>", print)
+        self.canvas.grid(sticky="NEWS")
 
     def setTextEditor(self, text):
         self.textEditor = text
 
 
-class TextEditor(tk.Frame):
-    def __init__(self):
-        pass
+class TextEditor(AutoFrame):
+    def create_widgets(self):
+        self.text = tk.Text(self, undo=True)
+        self.text.bind("<<Modified>>", print)
+        self.text.grid(sticky="NEWS")
 
     def setGraphicalEditor(self, graph):
         self.graphicalEditor = graph
 
 
-class Window(tk.Tk):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.title("Ellipses")
-        self.columnconfigure(0, weight=1)
-        self.rowconfigure(0, weight=1)
-        graphFrame = GraphicalEditor(self)
-        textFrame = TextEditor(self)
+class Window(AutoFrame):
+    def __init__(self, master=None, **kwargs):
+        super().__init__(master, **kwargs)
+        self.master.columnconfigure(0, weight=1)
+        self.master.rowconfigure(0, weight=1)
+        self.master.title("Ellipses")
+
+    def create_widgets(self):
+        graphFrame = GraphicalEditor(master=self)
+        textFrame = TextEditor(master=self)
+        textFrame.grid(row=0, column=0)
+        graphFrame.grid(row=0, column=1)
         graphFrame.setTextEditor(textFrame)
         textFrame.setGraphicalEditor(graphFrame)
 
 
 if __name__ == '__main__':
-    app = Window()
+    app = Window(master=tk.Tk())
     app.mainloop()
