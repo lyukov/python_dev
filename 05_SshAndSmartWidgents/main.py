4cde16bdaeb396074c27054c0c58b071abe3edda
Dmitry Lyukov <d@lyukov.com>
2021-03-28 22:46:26 +0300

    Colorchooser and clear button added

--- 05_SshAndSmartWidgents/main.py
+++ 05_SshAndSmartWidgents/main.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import tkinter.colorchooser
 import re
 from enum import Enum
 
@@ -6,14 +7,14 @@ from enum import Enum
 object_description = re.compile(
     r"^oval "
     r"\<"
-    r"(?P<x0>[\d\.]+) "
-    r"(?P<y0>[\d\.]+) "
-    r"(?P<x1>[\d\.]+) "
-    r"(?P<y1>[\d\.]+)"
+    r"(?P<x0>\-?[\d\.]+) "
+    r"(?P<y0>\-?[\d\.]+) "
+    r"(?P<x1>\-?[\d\.]+) "
+    r"(?P<y1>\-?[\d\.]+)"
     r"\> "
     r"(?P<width>[\d\.]+) "
-    r"(?P<outline>#[0123456789ABCDEF]{6}) "
-    r"(?P<fill>#[0123456789ABCDEF]{6})$"
+    r"(?P<outline>#[\dA-Fa-f]{6}) "
+    r"(?P<fill>#[\dA-Fa-f]{6})$"
 )
 
 
@@ -39,52 +40,62 @@ class AutoFrame(tk.Frame):
 
 
 class GraphicalEditor(AutoFrame):
+    def __init__(self, master=None, **kwargs):
+        super().__init__(master, **kwargs)
+        self.rowconfigure(1, weight=0)
+        self.__state = EditorState.FREE
+        self.__object = None
+        self.__coords = None
+        self.__fill = "#55c600"
+        self.__outline = "#000000"
+
     def create_widgets(self):
         self.canvas = tk.Canvas(self)
-        self.canvas.bind("<Button-1>", self.onPress)
-        self.canvas.bind("<ButtonRelease-1>", self.onRelease)
-        self.canvas.bind("<Motion>", self.onMotion)
-        self.canvas.grid(sticky="NEWS")
-        self.state = EditorState.FREE
-        self.object = None
-        self.coords = None
-        self.fill = "#000000"
-        self.outline = "#FFFFFF"
+        self.canvas.bind("<Button-1>", self.__onPress)
+        self.canvas.bind("<ButtonRelease-1>", self.__onRelease)
+        self.canvas.bind("<Motion>", self.__onMotion)
+        self.canvas.grid(row=0, columnspan=3, sticky="NEWS")
+        self.fillColorButton = tk.Button(self, text="Fill color", command=self.__newFillColor)
+        self.fillColorButton.grid(row=1, column=0, sticky="NEWS")
+        self.outlineColorButton = tk.Button(self, text="Outline color", command=self.__newOutlineColor)
+        self.outlineColorButton.grid(row=1, column=1, sticky="NEWS")
+        self.clearButton = tk.Button(self, text="Clear", command=self.__clear)
+        self.clearButton.grid(row=1, column=2, sticky="NEWS")
 
     def setTextEditor(self, text):
         self.textEditor = text
 
-    def onPress(self, event):
+    def __onPress(self, event):
         x, y = event.x, event.y
-        self.coords = x, y
+        self.__coords = x, y
         overlapping = self.canvas.find_overlapping(x, y, x, y)
         if overlapping:
-            self.state = EditorState.EDIT
-            self.object = overlapping[-1]
+            self.__state = EditorState.EDIT
+            self.__object = overlapping[-1]
         else:
-            self.state = EditorState.NEW
-            self.object = self.canvas.create_oval(
+            self.__state = EditorState.NEW
+            self.__object = self.canvas.create_oval(
                 x, y, x, y,
-                fill=self.fill,
-                outline=self.outline
+                fill=self.__fill,
+                outline=self.__outline
             )
 
-    def onMotion(self, event):
-        if self.state == EditorState.FREE:
+    def __onMotion(self, event):
+        if self.__state == EditorState.FREE:
             return
-        elif self.state == EditorState.NEW:
-            self.canvas.coords(self.object, *self.coords, event.x, event.y)
+        elif self.__state == EditorState.NEW:
+            self.canvas.coords(self.__object, *self.__coords, event.x, event.y)
         else:
-            x0, y0 = self.coords
+            x0, y0 = self.__coords
             x, y = event.x, event.y
-            self.canvas.move(self.object, x - x0, y - y0)
-            self.coords = x, y
+            self.canvas.move(self.__object, x - x0, y - y0)
+            self.__coords = x, y
 
-    def onRelease(self, event):
-        self.state = EditorState.FREE
-        self.updateText()
+    def __onRelease(self, event):
+        self.__state = EditorState.FREE
+        self.__updateText()
 
-    def dumpObject(self, object):
+    def __dumpObject(self, object):
         obj_type       = self.canvas.type(object)
         x0, y0, x1, y1 = self.canvas.coords(object)
         width          = self.canvas.itemcget(object, "width")
@@ -92,22 +103,31 @@ class GraphicalEditor(AutoFrame):
         fill           = self.canvas.itemcget(object, "fill")
         return f"{obj_type} <{x0} {y0} {x1} {y1}> {width} {outline} {fill}"
 
-    def updateText(self):
-        description = "\n".join(map(self.dumpObject, self.canvas.find_all()))
+    def __updateText(self):
+        description = "\n".join(map(self.__dumpObject, self.canvas.find_all()))
         self.textEditor.text.delete("1.0", tk.END)
         self.textEditor.text.insert("1.0", description)
+
+    def __newFillColor(self):
+        self.__fill = tk.colorchooser.askcolor(color=self.__fill)[-1]
+
+    def __newOutlineColor(self):
+        self.__outline = tk.colorchooser.askcolor(color=self.__outline)[-1]
+
+    def __clear(self):
+        self.textEditor.text.delete("1.0", tk.END)
 
 
 class TextEditor(AutoFrame):
     def create_widgets(self):
         self.text = tk.Text(self, undo=True)
-        self.text.bind("<<Modified>>", self.onModify)
+        self.text.bind("<<Modified>>", self.__onModify)
         self.text.grid(sticky="NEWS")
 
     def setGraphicalEditor(self, graph):
         self.graphicalEditor = graph
 
-    def onModify(self, event):
+    def __onModify(self, event):
         self.graphicalEditor.canvas.delete(tk.ALL)
         lines = self.text.get("1.0", tk.END).split("\n")
         for line in lines:
@@ -129,14 +149,15 @@ class Window(AutoFrame):
         self.master.columnconfigure(0, weight=1)
         self.master.rowconfigure(0, weight=1)
         self.master.title("Ellipses")
+        self.columnconfigure(1, weight=4)
 
     def create_widgets(self):
-        graphFrame = GraphicalEditor(master=self)
-        textFrame = TextEditor(master=self)
-        textFrame.grid(row=0, column=0)
-        graphFrame.grid(row=0, column=1)
-        graphFrame.setTextEditor(textFrame)
-        textFrame.setGraphicalEditor(graphFrame)
+        self.graphFrame = GraphicalEditor(master=self)
+        self.textFrame = TextEditor(master=self)
+        self.textFrame.grid(row=0, column=0, sticky="NEWS")
+        self.graphFrame.grid(row=0, column=1, sticky="NEWS")
+        self.graphFrame.setTextEditor(self.textFrame)
+        self.textFrame.setGraphicalEditor(self.graphFrame)
 
 
 if __name__ == '__main__':
